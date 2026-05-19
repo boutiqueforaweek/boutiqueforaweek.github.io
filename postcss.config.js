80edf4e4d1dc0a3ef3ee67786cdc15df03a6ab9a
Albert Volkman <albert.volkman@gmail.com>
2026-05-19 16:14:56 -0400

    Switch away from gulp

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+import { purgeCSSPlugin } from '@fullhuman/postcss-purgecss';
+import discardComments from 'postcss-discard-comments';
+import cssnano from 'cssnano';
+
+export default {
+  plugins: [
+    purgeCSSPlugin({ content: ['_site/**/*.html'] }),
+    discardComments({ removeAll: true }),
+    cssnano({ preset: 'default' }),
+  ],
+};
